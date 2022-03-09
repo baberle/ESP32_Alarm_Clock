14e936b3b54ea1b7a6835e74f95803c0082ed920
Brett Aberle <brett10a@gmail.com>
2022-03-09 10:00:17 -0500

    connected graphics library with simple test

--- include/alarmclock.h
+++ include/alarmclock.h
@@ -4,7 +4,23 @@
 #include <Arduino.h>
 #include <time.h>
 #include <WiFi.h>
+#include <GxEPD2_BW.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 #include "alarmplayer.h"
 #include "alarm.h"
+#include "AiEsp32RotaryEncoder.h"
+#include "bitmaps.h"
+#include <Adafruit_I2CDevice.h> 
+
+#define ROTARY_ENCODER_A_PIN 32
+#define ROTARY_ENCODER_B_PIN 21
+#define ROTARY_ENCODER_BUTTON_PIN 25
+#define ROTARY_ENCODER_VCC_PIN -1
+#define ROTARY_ENCODER_STEPS 4
+
+#define EINK_CS SS
+#define EINK_DC 17
+#define EINK_RST 16
+#define EINK_BUSY 4
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,18 @@ const int   daylightOffset_sec = 3600;
 const char* hostname = "ESP32 Alarm Clock";
 const bool WiFiEnabled = true;
 
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+
+GxEPD2_BW<GxEPD2_290, GxEPD2_290::HEIGHT> display(GxEPD2_290(EINK_CS, EINK_DC, EINK_RST, EINK_BUSY)); // GDEH029A1 128x296
+
 void setupWiFi();
 void WiFiConnect();
+void drawWiFiIcon();
+
+void IRAM_ATTR readEncoderISR()
+{
+    rotaryEncoder.readEncoder_ISR();
+}
 
 void setup() {
   Serial.begin(115200);
@@ -22,6 +32,16 @@ void setup() {
   setupDFPlayer();
   al.hour = 9;
   al.minute = 6;
+
+  rotaryEncoder.begin();
+  rotaryEncoder.setup(readEncoderISR);
+  rotaryEncoder.setBoundaries(0, 1000, false); //minValue, maxValue, circleValues true|false (when max go to min and vice versa)
+  rotaryEncoder.setAcceleration(250);
+
+  display.init();
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);  
 }
 
 void WiFiConnect() {
@@ -51,7 +71,7 @@ void setupWiFi() {
 }
 
 
-
+int alreadyDrawn = false;
 void loop() 
 {
   manageDFPlayer();
@@ -61,5 +81,36 @@ void loop()
     return;
   }
   al.checkAlarm(timeinfo);
-  delay(100);
+  if (rotaryEncoder.encoderChanged())
+  {
+      Serial.println(rotaryEncoder.readEncoder());
+  }
+  if (rotaryEncoder.isEncoderButtonClicked())
+  {
+      Serial.println("button pressed");
+  }
+  if(!alreadyDrawn) {
+    drawWiFiIcon();
+    alreadyDrawn = true;
+  }
+}
+
+// Draws the wifi icon at the top of the screen according to the wifi status
+void drawWiFiIcon() {
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    if(!WiFiEnabled) {
+      display.drawBitmap(296-24-1, 1, wifi_airplane_mode, 24, 24, GxEPD_BLACK);
+    } else {
+        if (WiFi.status() == WL_CONNECTED) 
+      {
+        display.drawBitmap(296-24-1, 1, wifi_connected, 24, 24, GxEPD_BLACK);
+      } else {
+        display.drawBitmap(296-24-1, 1, wifi_disconnected, 24, 24, GxEPD_BLACK);
+      }
+    }
+  }
+  while (display.nextPage());
 }
